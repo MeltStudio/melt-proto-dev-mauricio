fb2e2a4c92c4d6cae00bf30e75cb4a913416430c
Mauricio López <maurilo934@hotmail.com>
2025-07-10 12:30:38 -0500

    added general sync from subframe

--- src/ui/layouts/DefaultPageLayout.tsx
+++ src/ui/layouts/DefaultPageLayout.tsx
@@ -44,7 +44,71 @@ const DefaultPageLayoutRoot = React.forwardRef<
       ref={ref as any}
       {...otherProps}
     >
-
+      <SidebarRailWithIcons
+        header={
+          <div className="flex flex-col items-center justify-center gap-2 px-1 py-1">
+            <img
+              className="h-6 w-6 flex-none object-cover"
+              src="https://res.cloudinary.com/subframe/image/upload/v1711417507/shared/y2rsnhq3mex4auk54aye.png"
+            />
+          </div>
+        }
+        footer={
+          <>
+            <SidebarRailWithIcons.NavItem icon={<FeatherBell />}>
+              Item
+            </SidebarRailWithIcons.NavItem>
+            <SidebarRailWithIcons.NavItem icon={<FeatherSettings />}>
+              Item
+            </SidebarRailWithIcons.NavItem>
+            <div className="flex flex-col items-center justify-end gap-1 px-1 py-1">
+              <SubframeCore.DropdownMenu.Root>
+                <SubframeCore.DropdownMenu.Trigger asChild={true}>
+                  <Avatar
+                    size="small"
+                    image="https://res.cloudinary.com/subframe/image/upload/v1711417507/shared/fychrij7dzl8wgq2zjq9.avif"
+                  >
+                    A
+                  </Avatar>
+                </SubframeCore.DropdownMenu.Trigger>
+                <SubframeCore.DropdownMenu.Portal>
+                  <SubframeCore.DropdownMenu.Content
+                    side="right"
+                    align="end"
+                    sideOffset={4}
+                    asChild={true}
+                  >
+                    <DropdownMenu>
+                      <DropdownMenu.DropdownItem icon={<FeatherUser />}>
+                        Profile
+                      </DropdownMenu.DropdownItem>
+                      <DropdownMenu.DropdownItem icon={<FeatherSettings />}>
+                        Settings
+                      </DropdownMenu.DropdownItem>
+                      <DropdownMenu.DropdownItem icon={<FeatherLogOut />}>
+                        Log out
+                      </DropdownMenu.DropdownItem>
+                    </DropdownMenu>
+                  </SubframeCore.DropdownMenu.Content>
+                </SubframeCore.DropdownMenu.Portal>
+              </SubframeCore.DropdownMenu.Root>
+            </div>
+          </>
+        }
+      >
+        <SidebarRailWithIcons.NavItem icon={<FeatherHome />} selected={true}>
+          Home
+        </SidebarRailWithIcons.NavItem>
+        <SidebarRailWithIcons.NavItem icon={<FeatherInbox />}>
+          Inbox
+        </SidebarRailWithIcons.NavItem>
+        <SidebarRailWithIcons.NavItem icon={<FeatherBarChart2 />}>
+          Reports
+        </SidebarRailWithIcons.NavItem>
+        <SidebarRailWithIcons.NavItem icon={<FeatherFolder />}>
+          Projects
+        </SidebarRailWithIcons.NavItem>
+      </SidebarRailWithIcons>
       {children ? (
         <div className="flex grow shrink-0 basis-0 flex-col items-start gap-2 self-stretch overflow-y-auto bg-default-background">
           {children}
